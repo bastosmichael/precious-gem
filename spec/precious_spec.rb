b9ca4aa860cc9d75cd68fc92a72b545391cf7492
Michael Bastos <bastosmichael@gmail.com>
2022-04-09 18:25:39 -0500

    updating default spec

--- spec/precious_spec.rb
+++ spec/precious_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Precious do
   it "has a version number" do
     expect(Precious::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
