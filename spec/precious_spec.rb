b97140fcd6ff17f03763586e9fa4df3764a18c3a
Michael Bastos <bastosmichael@gmail.com>
2022-04-09 14:47:58 -0500

    first commit

---
+++ spec/precious_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Precious do
+  it "has a version number" do
+    expect(Precious::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
